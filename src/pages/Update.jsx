7547a52e83751b6963ae9d4f628ff49a5fc5df18
Abuh Solomon <53456402+Abuh05@users.noreply.github.com>
2022-11-18 16:43:11 +0100

    Feat: Added new Logic and style

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,9 +1,44 @@
+import axios from 'axios'
 import React from 'react'
+import { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const Update = () => {
+
+    const [input, setInput] = useState({
+        title:"",
+        desc:"",
+        price:null,
+        cover:"",
+    })
+
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const bookId = location.pathname.split("/")[2];
+
+    const handleClick = async (e) =>{
+        e.preventDefault()
+        try {
+            await axios.put("http://localhost:8800/books"+bookId, input)
+            navigate("/")
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    const handleChange = (e) =>{
+        setInput(prev=>({...prev, [e.target.name]: e.target.value}))
+    }
   return (
-    <div>Update</div>
+    <div className='form'>
+        <h1>Update the Book</h1>
+        <input type="text" placeholder='title' onChange={handleChange} name='title' />
+        <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+        <button className='formbtn' onClick={handleClick}>Update</button>
+    </div>
   )
 }
 
-export default Update
+export default Update;
